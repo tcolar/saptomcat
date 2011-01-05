dcd026299f885c02e569a3a3674cb848eab68de1
thibautc <thibautc@frenchie>
2011-01-05 09:10:20 -0800

    TextExperts support

---
+++ projects/sap-patches/src/com/pcf/sql/h2/H2TextExpert.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pcf.sql.h2;
+
+import com.sap.sql.tree.AbstractSqlTextExpert;
+import com.sap.sql.tree.SqlTextExpert;
+
+/**
+ * Nothing special so far
+ * @author thibautc
+ */
+public class H2TextExpert extends AbstractSqlTextExpert
+  implements SqlTextExpert
+{
+}
---
+++ projects/sap-patches/src/com/pcf/sql/mysql/MySqlTextExpert.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pcf.sql.mysql;
+
+import com.sap.sql.tree.AbstractSqlTextExpert;
+import com.sap.sql.tree.SqlTextExpert;
+
+/**
+ * Nothing special so far
+ * @author thibautc
+ */
+public class MySqlTextExpert extends AbstractSqlTextExpert
+  implements SqlTextExpert
+{
+}
